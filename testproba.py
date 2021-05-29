6a1d045af208138799edc680db1816cc1b58589d
ramzi <ramzi@ramzi.es>
2021-05-29 02:04:28 +0200

    commit all

---
+++ testproba.py
@@ -0,0 +1,14 @@
+import math
+
+
+#print(math.factorial(7)/(math.factorial(4)*math.factorial(3)))
+
+
+#*math.factorial(10)*math.factorial(59)))
+
+numb= 6*6*5
+
+print(((3*5*4)/numb)+((3*5*3)/numb))
+print(((5*5*3)/numb)+((6*5*1)/numb))
+
+# print(math.factorial(9))
